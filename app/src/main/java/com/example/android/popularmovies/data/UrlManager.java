867b79cadc23d8964f2a84d05c1b780f14f90879
Carlos de Castro <ca.decastrol@gmail.com>
2018-11-28 20:20:48 +0100

    PopularMovies stage two

--- app/src/main/java/com/example/android/popularmovies/data/UrlManager.java
+++ app/src/main/java/com/example/android/popularmovies/data/UrlManager.java
@@ -6,7 +6,7 @@ public class UrlManager {
     static final String API_HOST = "http://api.themoviedb.org/3/";
     static final String MOVIE = "movie/{orderBy}";
     // Put your API KEY here
-    public static final String KEY = "c963e64788327bb400809c736e1544cf";
+    public static final String KEY = "";
     static final String MOVIE_VIDEOS = "movie/{id}/videos";
     static final String MOVIE_REVIEWS = "movie/{id}/reviews";
 }
